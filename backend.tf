6a73d40c1d3c3df8252572684f62617d1d515374
Marek Szczegodziński <mszczegodzinski@o2.pl>
2023-04-04 00:16:57 +0200

    feat: backend.tf update

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 terraform {
   required_version = ">= 1.0.11"
-  backend "gcs" {
+  backend "task-7-terraform-state" {
     bucket = "task-7-terraform-state"
     prefix = "dev"
   }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
